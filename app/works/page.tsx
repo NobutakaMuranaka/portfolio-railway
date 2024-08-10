517a1541c02b70d2e0937d91227026110b80e174
村中信隆 <chanayataka2324@gmail.com>
2024-08-10 09:32:57 +0900

    背景の余白を縮小

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -4,7 +4,7 @@ import Card from "./components/Card/Card";
 export default function Works() {
   const data = swagData;
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 auto-rows-max	 gap-6 m-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 auto-rows-max	 gap-6 m-5">
       {data.map(({ id, title, imageSrc }) => (
         <Card key={id} id={id} title={title} imageSrc={imageSrc}></Card>
       ))}
