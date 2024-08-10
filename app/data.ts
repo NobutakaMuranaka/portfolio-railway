16b11c19b92e78863346c1f7a6476d9cfb058559
村中信隆 <chanayataka2324@gmail.com>
2024-08-10 11:13:56 +0900

    画像を取得できるよう修正

--- app/data.ts
+++ app/data.ts
@@ -10,18 +10,21 @@ export type Work = {
 export const fetchWorks = async (): Promise<Work[]> => {
   try {
     const response = await axios.get(
-      "https://your-project-id.microcms.io/api/v1/works",
+      "https://portfolio-railway.microcms.io/api/v1/works",
       {
         headers: {
           "X-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
         },
       }
     );
+
+    console.log(response.data); // ここでレスポンスデータを確認
+
     return response.data.contents.map((item: any) => ({
       workId: item.workId,
       title: item.title,
       description: item.description,
-      imageSrc: item.image.url,
+      imageSrc: item.imageSrc?.url || "", // imageSrcオブジェクトからURLを取得
     }));
   } catch (error) {
     console.error("MicroCMSからのデータ取得エラー:", error);
--- app/works/@modal/(..)works/[id]/page.tsx
+++ app/works/@modal/(..)works/[id]/page.tsx
@@ -1,13 +1,18 @@
 import CardDetail from "../../../components/CardDetail/CardDetail";
 import Modal from "../../../components/Modal/Modal";
-import swagData, { Work } from "../../../../data";
+import { fetchWorks, Work } from "../../../../data";
 
-export default function PhotoModal({
+export default async function PhotoModal({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const data: Work = swagData.find((p) => p.workId === id)!;
+  const works: Work[] = await fetchWorks();
+  const data = works.find((p) => p.workId === id);
+
+  if (!data) {
+    return <div>データが見つかりませんでした。</div>;
+  }
 
   return (
     <Modal>
