6c6ca199a06851a9b2b2fc6c78939aaa44c6e759
村中信隆 <chanayataka2324@gmail.com>
2024-08-12 20:50:06 +0900

    カードの表示を10件から100件に上限増加

--- app/data.ts
+++ app/data.ts
@@ -15,6 +15,9 @@ export const fetchWorks = async (): Promise<Work[]> => {
         headers: {
           "X-API-KEY": process.env.API_KEY,
         },
+        params: {
+          limit: 100,
+        },
       }
     );
 
